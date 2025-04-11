e287a5a92a21046ac84e935ac0c217aa9a0e6e81
Fernando Da Silva <ferda-si@c1r7s1.42luanda.com>
2025-04-11 19:15:48 +0100

    init fork and philos

--- philo/headers/philosopher.h
+++ philo/headers/philosopher.h
@@ -21,3 +21,35 @@ int	ft_isdigit_str(char *str);
 int	ft_isdigit(int c);
 void    check_philo_number(char **str);
 void check_valid_numbers(char **str);
+
+typedef struct s_fork{
+   int id;
+   pthread_mutex_t mutex;
+} t_fork;
+
+typedef struct s_philo t_philo;
+typedef struct s_data t_data;
+
+typedef struct s_philo {
+   int id;
+   pthread_t   thread;
+   int count_mealded;
+   t_fork *left_fork;
+   t_fork *right_fork;
+   t_data *data;
+} t_philo;
+
+
+typedef struct s_data {
+   t_philo *philo;
+   t_fork *fork;
+   long  start_time;
+   int   time_to_die;
+   int   time_to_sleep;
+   int   number_philos;
+   int   time_to_eat;
+   int   someone_died;
+   int   number_must_eat;
+} t_data;
+
+
--- philo/src/main.c
+++ philo/src/main.c
@@ -1,18 +1,15 @@
 #include "philosopher.h"
 
-typedef struct philo {
-   int count_mealded;
-} s_philo;
 
-typedef struct t_data {
-   s_philo *philo;
-} s_data;
 
-s_data *get_data ()
+
+
+
+t_data *get_data ()
 {
-   static s_data *data_str = NULL;
+   static t_data *data_str = NULL;
    if (data_str == NULL)
-      data_str = (s_data*) malloc(sizeof(s_data));
+      data_str = (t_data*) malloc(sizeof(t_data));
    return (data_str);
 }
 
@@ -21,14 +18,72 @@ void philopher_doing_thins()
    printf("%d\n",get_data()->philo->count_mealded);
 }
 
+
+void init_forks(t_data *data){
+   int	i;
+   t_fork	*fork;
+
+   fork = (t_fork*)malloc(sizeof(t_fork) * data->number_philos);
+   i = 0;
+   while (i < data->number_philos)
+   {
+		fork[i].id = i+1;
+		pthread_mutex_init(&fork[i].mutex, NULL);
+		i++;
+   }
+   data->fork = fork;
+}
+
+
+void init_philos(t_data *data){
+   int	i;
+   t_philo	*philo;
+
+   philo = (t_philo*)malloc(sizeof(t_philo) * data->number_philos);
+   i = 0;
+   while (i < data->number_philos)
+   {
+		philo[i].id = i+1;
+		philo[i].left_fork = &data->fork[i];
+		philo[i].right_fork = &data->fork[(i + 1) % data->number_philos];
+		philo[i].data = data;
+		i++;
+   }
+   data->philo = philo;
+}
+
+void print_forks(t_data data)
+{
+	for (int i = 0; i < data.number_philos; i++)
+	{
+		printf("fork %d \n", data.fork[i].id);
+	}
+}
+
+void print_philos(t_data data)
+{
+	for (int i = 0; i < data.number_philos; i++)
+	{
+		printf("philo %d \n", data.philo[i].id);
+	}
+}
+
+void routine_philosopher()
+{
+
+}
+
+
 int main(int ac, char **av)
 {
-   s_philo *philo = NULL;
-   philo = (s_philo*)malloc(sizeof(s_philo));
-   philo->count_mealded = 42;
-   get_data()->philo = philo;
-   philopher_doing_thins();
-   exit(0);
+  // t_philo *philo = NULL;
+   t_data data;
+   init_forks(&data);
+   init_philos(&data);
+   //routine
+   //monitoring
+	exit(0);
+
    ft_check_args_quant(ac);
    check_valid_numbers(av);
    check_philo_number(av);
